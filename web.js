ea604e485bec9ef7f29bdad96a03e417f59e1d8f
abrugman <andres.brugman@gmail.com>
2013-07-12 13:59:34 +0000

    Added reading index.htlm file.

--- web.js
+++ web.js
@@ -3,10 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var prueba = fs.readFileSync('index.html','utf8')
+    response.send(prueba);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+

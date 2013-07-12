16f4ba60bdc11e125160a74700251405d5dfd68c
abrugman <andres.brugman@gmail.com>
2013-07-12 14:22:11 +0000

    Added new line require fs.

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+    var fs = require('fs');
     var prueba = fs.readFileSync('index.html','utf8')
     response.send(prueba);
 });
@@ -11,6 +12,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-
 

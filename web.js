6fdeb559311ea2771b17efc02f6d78fe9387dda3
Ubuntu <ubuntu@ip-172-31-12-49.us-west-2.compute.internal>
2013-07-17 04:15:35 +0000

    changed we.js to include port 8080

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var fs = require('fs');
-    var prueba = fs.readFileSync('index.html','utf8')
+    // var fs = require('fs');
+    var prueba = fs.readFileSync(htmlfile).toString();
     response.send(prueba);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });

3a4f6a6eca2d564dd69a4006a5a53b75a883dfaf
abrugman <andres.brugman@gmail.com>
2013-11-27 01:15:23 +0000

    introduces middleware to load d3 json as response. Needs fix an issue when loading fo first time, needs reload in chrome.

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-var htmlfile = "index.html";
 
 
 
+
+// var htmlfile = "index.html";
+var htmlfile = "start.html";
 app.get('/', function(request, response) {
     var prueba = fs.readFileSync(htmlfile).toString();
     response.send(prueba);
@@ -18,7 +20,8 @@ app.get('/', function(request, response) {
 
 /* Page 2 */
 
-var start_html = "start.html";
+// var start_html = "start.html";
+var start_html = "index.html";
 app.get('/start', function(req, res){
    res.send(fs.readFileSync(start_html).toString());
 });
@@ -49,26 +52,46 @@ app.get('/dataeve', function(req, res){
 });
 
 
-/* pbv Chart Data como pbv3.json */
+/* Processes httpgetrequest on ticker and stores result on file ./data/pbv.json by calling run_mod module */
 
-var datapbv = './data/pbv.json';
-app.get('/datapbvg', function(req, res) {
+app.get('/dataquery', function(req, res, next) {
     var ticker = req.query.q;    // guarda el ticker
     var run_mod = require('./run_mod.js');  // ejecuta la funcion run_mod con el parametro ticker = KO
     run_mod(ticker, function(data) {
 	console.log('contendio de data es: ' + data);
-	res.send(data);
+//	res.send(data);
+	next();
     });
+
 });
 
-
-
-/* pbv Chart */
+/* Middleware: serves new file pbv.html which loads d3 visualization 
+   with pbv information in json format from newly created file pbv.json*/
 
 var pbvk = "pbv.html";
-app.get('/pbvg', function(req, res){
-   res.send(fs.readFileSync(pbvk).toString());
+app.get('/dataquery', function(req, res, next) {
+    console.log ('imprimio prueba!!!!!!!!!!!!!!!!!!!!!!!!!!');
+    res.send(fs.readFileSync(pbvk).toString()); // NO SERIA MEJOR HACER UN REDIRECT A /pbvg porque no esta cargando la primera vez en chrome con el request, solo con un reload...
 });
+
+/* serves ./data/pbv.json file that is requested in pbv.html graph */ 
+
+var datapbv = "./data/pbv.json";
+app.get('/datapbv', function(req, res, next){
+    res.send(fs.readFileSync(datapbv).toString());
+});
+
+
+
+//app.get('/pbvg', function(req, res, next){
+//    res.send(fs.readFileSync(pbvk).toString());
+//});
+
+
+
+
+
+
 
 
 
